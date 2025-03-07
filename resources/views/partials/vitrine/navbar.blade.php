4463b2d17ab9c15addd543dff2a7ecc0b84c42e8
Quentin <quentinsacla@gmail.com>
2025-03-07 13:12:09 +0100

    commit 3

--- resources/views/partials/vitrine/navbar.blade.php
+++ resources/views/partials/vitrine/navbar.blade.php
@@ -1,24 +1,21 @@
-<header id="header" class="header sticky-top bg-white shadow-sm">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between py-2">
+ <header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
-      <!-- Logo -->
-      <a href="{{ route('website.index') }}" class="logo d-flex align-items-center gap-2">
-        <img src="assets/img/wplogo.png" alt="Logo" height="50">
+      <a href="{{ route('website.index') }}" class="logo d-flex align-items-center">
+        <img src="assets/img/wplogo.png" alt="">
+
       </a>
 
-      <!-- Navigation -->
-      <nav id="navmenu" class="navmenu d-flex align-items-center">
-        <ul class="d-flex list-unstyled m-0 gap-4">
-          <li><a href="{{ route('website.index') }}" class="active">Accueil</a></li>
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="{{ route('website.index') }}" class="active">Acceuil</a></li>
           <li><a href="#about">À propos</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="{{ route('website.team') }}">Équipe</a></li>
           <li><a href="{{ route('website.contact') }}">Contact</a></li>
           <li><a href="{{ route('website.job') }}">Recrutements</a></li>
         </ul>
-        <button class="mobile-nav-toggle d-xl-none border-0 bg-transparent" aria-label="Menu">
-          <i class="bi bi-list fs-3"></i>
-        </button>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
 
     </div>
